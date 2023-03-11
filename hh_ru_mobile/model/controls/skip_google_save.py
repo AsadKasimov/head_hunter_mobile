7f45cc77c7fdb9cdcb7db3c1b68c2853cf74ad63
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2023-03-12 02:55:53 +0300

    first commit

--- hh_ru_mobile/model/controls/skip_google_save.py
+++ hh_ru_mobile/model/controls/skip_google_save.py
@@ -5,6 +5,6 @@ from selene.support.shared import browser
 
 
 def skip_google_save():
-    with allure.step(f'Закрыть окно авторизации'):
-        if browser.element((AppiumBy.ID, "android:id/autofill_save_no")).matching(be.visible):
+    with allure.step(f'Закрыть окно сохранения логина/пароля'):
+        if browser.element((AppiumBy.ID, "android:iнd/autofill_save_no")).matching(be.visible):
             browser.element('#fragment_onboarding_skip_button').tap()
--- hh_ru_mobile/model/controls/skip_login.py
+++ hh_ru_mobile/model/controls/skip_login.py
@@ -1,8 +1,11 @@
 import time
+
+import allure
 from appium.webdriver.common.appiumby import AppiumBy
 from selene.support.shared import browser
 
 def skip_login():
-    time.sleep(2)
-    browser.element(
-        (AppiumBy.ID, "ru.hh.android:id/fragment_intentions_onboarding_choose_direction_image_close")).click()
+    with allure.step('Закрыть окно авторизации'):
+        time.sleep(2)
+        browser.element(
+            (AppiumBy.ID, "ru.hh.android:id/fragment_intentions_onboarding_choose_direction_image_close")).click()
